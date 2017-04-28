f8a6dad614e85c49554f833ae73970cbf77922a9
Tommy Ku <zoo1929@outlook.com>
2017-04-29 02:54:27 +0800

    ADD: be able to fetch route, stop and eta information using Telegram command

--- kmb-eta-unofficial-bot.rb
+++ kmb-eta-unofficial-bot.rb
@@ -2,6 +2,8 @@
 require 'telegram/bot'
 require 'envyable'
 require 'awesome_print'
+require_relative 'kmb/get_stops.rb'
+require_relative 'kmb/get_eta.rb'
 
 Envyable.load(File.expand_path('env.yml', File.dirname( __FILE__)))
 
@@ -11,6 +13,15 @@ def handle_command(message)
   case command
   when /\/start/i
     @bot.api.send_message(chat_id: message.chat.id, text: 'Your voice will be heard.')
+  when /\/route/i
+    response = handle_route(param)
+    @bot.api.send_message(chat_id: message.chat.id, text: response) if response
+  when /\/stops/i
+    response = handle_stop(param)
+    @bot.api.send_message(chat_id: message.chat.id, text: response) if response
+  when /\/eta/i
+    response = handle_eta(param)
+    @bot.api.send_message(chat_id: message.chat.id, text: response) if response
   end
 end
 
@@ -27,6 +38,63 @@ end
 
 def handle_message(message)
   # pass
+end
+
+def handle_route(param)
+  route = param
+  if route =~ /^[a-z0-9]+$/i
+    kmbGetStops = Kmb::GetStops.new(route, '1')
+    if basic_info = kmbGetStops.basic_info
+      orig = basic_info['OriCName']
+      dest = basic_info['DestCName']
+      "if you are going from #{dest} to #{orig}, add '2' when you call /stops or /eta, otherwise, there is no need to add '2'"
+    else
+      'wait something is wrong'
+    end
+  else
+    'are you sure this is a route?'
+  end
+end
+
+def handle_stop(param)
+  route, bound = param.split(' ', 2)
+  bound ||= '1'
+  if route =~ /^[a-z0-9]+$/i
+    kmbGetStops = Kmb::GetStops.new(route, bound)
+    if route_stops = kmbGetStops.route_stops
+      response = "To see the ETA to these stations, use these code\n"
+      route_stops.each do |stop|
+        response += "#{stop['CName']} #{stop['BSICode'].gsub('-', '')}\n"
+      end
+      eta_command = "/eta #{route} #{route_stops[0]['BSICode'].gsub('-', '')}"
+      eta_command += bound == '1' ? '' : ' 2'
+      response += "If you want to know the ETA to #{route_stops[0]['CName']}, type '#{eta_command}'"
+      response
+    else
+      'wait something is wrong'
+    end
+  else
+    'are you sure this is a route?'
+  end
+end
+
+def handle_eta(param)
+  route, bsi, bound = param.split(' ', 3)
+  bound ||= '1'
+  if route =~ /^[a-z0-9]+$/i
+    kmbGetEta= Kmb::GetETA.new(route, bound, bsi)
+    if eta = kmbGetEta.eta
+      response = ''
+      eta.each do |e|
+        response += "#{e['t']}\n"
+      end
+      response
+    else
+      'wait something is wrong'
+    end
+  else
+    'are you sure this is a route?'
+  end
 end
 
 Telegram::Bot::Client.run(ENV['TELEGRAM_BOT_TOKEN']) do |bot|
